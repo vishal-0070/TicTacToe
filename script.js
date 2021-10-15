adc185fb637bc13d672cf7c3c7a7501bfa3bcd9f
Vishal Mangla <76559165+vishal-0070@users.noreply.github.com>
2021-10-15 17:25:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,6 +96,7 @@ const checkWin = ()=>{
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
             isgameover = true
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            gameover.play();
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
             document.querySelector(".line").style.width = "20vw";
         }
